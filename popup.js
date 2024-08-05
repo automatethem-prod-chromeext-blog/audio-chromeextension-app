5848f5d4e35454ed673cd60b5d4b6d3804ce0d1c
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-08-05 15:17:25 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,7 @@
 console.log("I am popup.js");
 
 const button = document.querySelector('#button');
-//const button = document.getElementById("button");
-button.addEventListener("click", async () => {
+button.addEventListener("click", () => {
   const audio = new Audio(chrome.runtime.getURL("bell.wav"));
   audio.play();
 });
